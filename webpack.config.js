2d36bf54e3cc17d709eec3c94d065d0b53a2df37
sergiolvargas95 <65960993+sergiolvargas95@users.noreply.github.com>
2021-07-04 13:20:03 -0500

    optimización de imagenes y fuentes

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]',
     },
     resolve: {
         extensions: ['.js']
@@ -21,11 +23,30 @@ module.exports = {
                 exclude: /node-modules/
             },
             {
-                test: /\.css$/i,
+                test: /\.css|.styl$/i,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
+                    'css-loader',
+                    'stylus-loader'
+                ],
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[ext]",
+                        outputPath: './assets/fonts/',
+                        publicPath: './assets/fonts/',
+                        esModule: false,
+                    },
+                }
             }
         ]
     },
@@ -36,5 +57,13 @@ module.exports = {
             filename: './index.html'
         }),
         new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        })
     ]
 }
